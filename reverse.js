a398e6fad72c596b7743001b2ed4069458e9c2c7
Akhilesh yadav <akhileshyadav310596@gmail.com>
2022-12-18 08:09:33 +0530

    recursive

--- reverse.js
+++ reverse.js
@@ -13,3 +13,19 @@ function isPaildrome(str){
 
 let res=isPaildrome("aka")
 console.log(res)
+
+const isOdd = val => val % 2 !== 0;
+
+
+
+
+function someRecursive(array,isOdd){
+ 
+   if (array.length === 0) return false;
+    if (isOdd(array[0])) return true;
+    return someRecursive(array.slice(1),isOdd);
+}
+let res1=someRecursive([1,2,3,4], isOdd) // true
+someRecursive([4,6,8,9], isOdd) // true
+someRecursive([4,6,8], isOdd) // false
+console.log(res1)
